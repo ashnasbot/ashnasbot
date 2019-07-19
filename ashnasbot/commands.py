6f4b410b841984b2e77620e21c8b8dccb8ad2888
Ashnas <jp@ashnas.net>
2019-07-19 20:27:10 +0100

    Add db for deaths

--- ashnasbot/commands.py
+++ ashnasbot/commands.py
@@ -1,5 +1,9 @@
 import random
 
+import dataset
+
+db = dataset.connect('sqlite:///ashnasbot.db')
+
 def no_cmd(event, who, *args):
     remainder = " ".join(args)
     event["message"] = f"No {who} {remainder}"
@@ -49,3 +53,43 @@ def praise_cmd(event, praise, *args):
     message = " ".join([praise, *args])
     event["message"] = f"praise {praise} - {ending.format(praise=message)}"
     return event
+
+def death_cmd(event, *args):
+    table = db["channel"]
+    try:
+        data = table.find_one(channel=event["channel"])
+        print(data)
+        if data == None:
+            data = {"channel": event["channel"], "deaths":0}
+        DEATHS = data["deaths"]
+    except:
+        DEATHS = 0
+
+    plus = " ".join(args)
+    if event.tags['caller'] == 'Ashnas' or event.tags['caller'] == 'Darkshoxx' or \
+        event.tags['caller'] == 'TheADrain':
+        update = True
+        if plus == "":
+            update = False
+        elif plus == "++":
+            DEATHS += 1
+        elif plus == "--":
+            DEATHS -= 1
+        else:
+            try:
+                DEATHS = int(plus)
+            except ValueError:
+                update = False
+                event["message"] = f"/me unknown deaths value: {plus}"
+                return event
+        if update:
+            data["deaths"] = DEATHS
+            print("saving:", data)
+            table.upsert(data, ["channel"], ensure=True)
+
+    times = "times"
+    if DEATHS == 1:
+        times = "time"
+    event["message"] = f"/me Our illustrious strimmer has died {DEATHS} {times}"
+    return event
+

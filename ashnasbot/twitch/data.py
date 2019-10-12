e544f85f78291f1bfe51c4cb98b2731334667b97
Ashnas <jp@ashnas.net>
2019-10-12 18:32:48 +0100

    Add founder badge

--- ashnasbot/twitch/data.py
+++ ashnasbot/twitch/data.py
@@ -19,7 +19,8 @@ BADGES = {
     'turbo': STATIC_CDN + "chat-badges/turbo-alpha.png",
     'partner': STATIC_CDN + "badges/v1/d12a2e27-16f6-41d0-ab77-b780518f00a3/2",
     'premium': STATIC_CDN + "badges/v1/a1dd5073-19c3-4911-8cb4-c464a7bc1510/2",
-    'vip': STATIC_CDN + "badges/v1/b817aba4-fad8-49e2-b88a-7cc744dfa6ec/2"
+    'vip': STATIC_CDN + "badges/v1/b817aba4-fad8-49e2-b88a-7cc744dfa6ec/2",
+    'founder': STATIC_CDN + "badges/v1/511b78a9-ab37-472f-9569-457753bbe7d3/2"
 }
 
 SUB_TIERS = [0, 3, 6, 9, 12, 18, 24, 30]

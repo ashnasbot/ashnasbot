afec5bae1b68a67c9f77cdb29e0cffd5d7f43383
Ashnas <ashnas@ashnas.net>
2023-02-26 20:55:43 +0000

    Chatbot 3.0

--- ashnasbot/twitch/chatter.py
+++ ashnasbot/twitch/chatter.py
@@ -27,78 +27,65 @@ class ChatChatter():
             self.add_event(evt)
 
     async def handle_message(self, event, cid=None):
+        evt = self.make_message(event.channel)
         bttv_emotes = await bttv.get_emotes(cid)
+        generated = False
+        content = {}
         if event.message in OWN_EMOTES:
             if random.randint(1, 3) == 1:
                 emote = event.message
-                evt = self.make_message(event.channel, emote)
+                evt.message = emote
                 url = OWN_EMOTES[emote][1]
                 evt.orig_message = EMOTE_FULL_TEMPLATE.format(url=url, alt=emote)
-                await self.add_event(evt)
-                logger.debug("RESPONSE %s %s", evt.message, evt)
-                return
 
         elif event.message in bttv_emotes:
-            emote = event.message
-            evt = self.make_message(event.channel, emote)
-            url = bttv_emotes[emote]
-            evt.orig_message = url
-            await self.add_event(evt)
-            logger.debug("RESPONSE %s %s", evt.message, evt)
-            return
+            if random.randint(1, 3) == 1:
+                emote = event.message
+                evt.message = emote
+                url = bttv_emotes[emote]
+                evt.orig_message = url
 
-        reply = False
-        if 'reply-parent-user-login' in event.tags and \
+        elif event.message.lower().startswith("@ashnasbot"):
+            commands.common.chat_cmd(content, event.message[10:])
+            evt.tags["reply-parent-msg-id"] = event.tags["id"]
+            evt.message = content["message"]
+            generated = True
+
+        elif 'reply-parent-user-login' in event.tags and \
                 event.tags['reply-parent-user-login'] == "ashnasbot":
-            reply = True
-
-        if "ashnasbot" in event.message.lower() or reply:
-
-            evt = self.make_message(event.channel)
+            generated = True
             if event.message.lower().strip('@') == "ashnasbot yes":
                 evt.message = ":)"
-                await self.add_event(evt)
-                return
             elif event.message.lower().strip('@') == "ashnasbot no":
                 evt.message = ":("
-                await self.add_event(evt)
-                return
+            else:
+                evt.tags["reply-parent-msg-id"] = event.tags["id"]
+                wordlist = event.message.translate(str.maketrans('', '', string.punctuation)).split(" ")
+                shortlist = [i for i in wordlist if i.lower() != "ashnasbot"]
+                if not shortlist:
+                    shortlist = wordlist
+                prompt = random.choice(shortlist)
+                if random.randint(1, 3) <= 2:
+                    prompt += " "
 
-            evt.tags["reply-parent-msg-id"] = event.tags["id"]
-            wordlist = event.message.translate(str.maketrans('', '', string.punctuation)).split(" ")
-            shortlist = [i for i in wordlist if i.lower() != "ashnasbot"]
-            if not shortlist:
-                shortlist = wordlist
-            prompt = random.choice(shortlist)
-            if random.randint(1, 3) <= 2:
-                prompt += " "
-
-            # !chat expects an output event - i.e. dict based
-            content = {}
-            commands.common.chat_cmd(content, prompt)
-            evt.message = content["message"]
-
-            await self.add_event(evt)
-            logger.debug("RESPONSE %s %s", evt.message, evt)
-            return
+                commands.common.chat_cmd(content, prompt)
+                evt.message = content["message"]
 
         # 1/30 borrowed from buttsbot
-        if random.randint(1, 30) == 1:
-            evt = self.make_message(event.channel)
+        elif random.randint(1, 30) == 1:
             wordlist = event.message.translate(str.maketrans('', '', string.punctuation)).split(" ")
             shortlist = [i for i in wordlist]
             if not shortlist:
                 shortlist = wordlist
             prompt = random.choice(shortlist)
 
-            # !chat expects an output event - i.e. dict based
-            content = {}
             commands.common.chat_cmd(content, prompt)
             evt.message = content["message"]
+            generated = True
 
-            await self.add_event(evt)
+        if generated:
             logger.debug("RESPONSE %s %s", evt.message, evt)
-            return
+            await self.add_event(evt)
 
     def make_message(self, channel, message=""):
         msg = create_event('TWITCHCHATMESSAGE', message)
